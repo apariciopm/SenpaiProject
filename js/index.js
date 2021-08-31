a48dd332848c5dd31e9e758e8475f74374af3aa6
Aparicio Pineyrua <apariciopineyrua@192.168.1.9>
2021-08-31 20:28:25 -0300

    js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let precio = 10;
+let iva = 1.22;
+
+precio = 100;
+
+const sumaIva = function (precio, iva) {
+    return precio * iva;
+}
+
+if (sumaIva > 100) {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(sumaIva(precio, iva));
+
