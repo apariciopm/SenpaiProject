4290aac6b9d0ee5f4bbacf8c5dda64b576266598
Aparicio Pineyrua <apariciopineyrua@192.168.1.8>
2021-09-05 17:24:24 -0300

    fetch

--- js/index.js
+++ js/index.js
@@ -85,20 +85,3 @@ if (number > 100 ) {
 else if (number < 200) {
   console.log("False");
 }
-
-
-
-/* 
-Desarrolle un algoritmo que permita leer tres valores y almacenarlos en las variables A, B y C
-respectivamente. El algoritmo debe imprimir cual es el mayor y cual es el menor. 
-Recuerde constatar que los tres valores introducidos por el teclado sean valores distintos. 
-Presente un mensaje de alerta en caso de que se detecte la introducción de valores iguales.
-*/
-
-let valorA = prompt ("Introducí un valor entero");
-let valorB = prompt ("Introducí un valor entero");
-let valorC = prompt ("Introducí un valor entero");
-
-if (valorA = valorB || valorB = valorC || valorC = valorA) {
-  
-}
